1985b8efc1c0d003745f0b26cc38a765f6d4fc34
MMS5-lang <miloszmattras@gmail.com>
2025-05-21 09:19:30 +0200

    GPIO oddane

--- Archives/GPIO/main.c
+++ Archives/GPIO/main.c
@@ -1,14 +1,14 @@
 #include <LPC21xx.H>
+
 #define LED0_bm 0x10000
 #define LED1_bm 0x20000
 #define LED2_bm 0x40000
 #define LED3_bm 0x80000
-#define ALL_LEDS_bm (LED0_bm | LED1_bm | LED2_bm | LED3_bm)
+
 #define BUTTON_S0_bm 0x10
 #define BUTTON_S1_bm 0x40
 #define BUTTON_S2_bm 0x20
 #define BUTTON_S3_bm 0x80
-#define ALL_BUTTONS_bm (BUTTON_S0_bm | BUTTON_S1_bm | BUTTON_S2_bm | BUTTON_S3_bm)
 
 enum eKeyboardState {RELASED,BUTTON_0,BUTTON_1,BUTTON_2,BUTTON_3};
 enum eDirection {LEFT, RIGHT};
@@ -22,31 +22,46 @@ void Delay(unsigned int uiMilliSeconds){
 }
 
 void LedInit(){
-	IO1DIR = IO1DIR | ALL_LEDS_bm;
+	IO1DIR = IO1DIR | (LED0_bm | LED1_bm | LED2_bm | LED3_bm);
 	IO1SET=LED0_bm;
 }
 
+
+
 void LedOn(unsigned char ucLedIndex) {
-    if (ucLedIndex > 3) {
-        return; 
-    }
-    IO1CLR = ALL_LEDS_bm;
-    IO1SET = (LED0_bm << ucLedIndex);
+	
+	IO1CLR = (LED0_bm | LED1_bm | LED2_bm | LED3_bm);
+	
+	switch(ucLedIndex){
+		case 0:
+			IO1SET = LED0_bm;
+			break;
+		case 1:
+			IO1SET = LED1_bm;
+			break;
+		case 2:
+			IO1SET = LED2_bm;
+			break;
+		case 3:
+			IO1SET = LED3_bm;
+		default:
+			break;
+	}
 }
-
-
+    
+		
 void KeyboardInit() {
-    IO0DIR = IO0DIR & ~ALL_BUTTONS_bm;
+    IO0DIR = IO0DIR & ~(BUTTON_S0_bm | BUTTON_S1_bm | BUTTON_S2_bm | BUTTON_S3_bm);
 }
 
 enum eKeyboardState eKeyboardRead(){
 	if((BUTTON_S0_bm & IO0PIN)==0){
 		return BUTTON_0;
 	}
-	else if ((BUTTON_S1_bm & IO0PIN)==0){
+	else if ((BUTTON_S1_bm & IO0PIN)==0){ //  0x40   0x0
 		return BUTTON_1;
 	}
-	else if ((BUTTON_S2_bm & IO0PIN)==0){
+	else if ((BUTTON_S2_bm & IO0PIN)==0){ //  0x20   0x0
 		return BUTTON_2;
 	}
 	else if ((BUTTON_S3_bm & IO0PIN)==0){
@@ -62,11 +77,11 @@ void LedStep(enum eDirection eDirection) {
 		static unsigned int uiCurrentLedIndex = 0;
 	
     if (eDirection == LEFT) {
-        uiCurrentLedIndex = (uiCurrentLedIndex + 1) % 4;
+        uiCurrentLedIndex = (uiCurrentLedIndex + 1);
     } else {
-        uiCurrentLedIndex = (uiCurrentLedIndex - 1) % 4;
+        uiCurrentLedIndex = (uiCurrentLedIndex - 1); //uc 0 odejmujemy 1 i ile bedzie
     }
-    LedOn(uiCurrentLedIndex);
+    LedOn(uiCurrentLedIndex % 4);
 }
 
 void LedStepLeft(void) {
@@ -80,6 +95,7 @@ void LedStepRight(void) {
 
 int main(){
 
+/*
 	LedInit();
 	KeyboardInit();
 	while (1) {
@@ -103,25 +119,25 @@ int main(){
                 break;
         }
     }
-
+*/
 /*
 		unsigned char ucStepCount = 0;
-		delay(1000);
+		Delay(1000);
     LedInit();
     KeyboardInit();
 
     while (1) {
         for (ucStepCount = 0; ucStepCount < 9; ucStepCount++) {
             LedStep(RIGHT);
-            delay(300);
+            Delay(300);
         }
         for (ucStepCount = 0; ucStepCount < 9; ucStepCount++) {
             LedStep(LEFT);
-            delay(300);
+            Delay(300);
         }
     }
-		*/
-		
+
+*/
 /*
 	enum eKeyboardState eKeyboardState;
 
