8de9126ee8cd77f552064129e0e9a62306941f81
Milosz M <miloszmattras@gmail.com>
2025-05-28 00:13:32 +0200

    final zmiany

--- Archives/Przerwania/timer.c
+++ Archives/Przerwania/timer.c
@@ -16,8 +16,8 @@ void InitTimer0(void){
 
 void WaitOnTimer0(unsigned int uiTime) {
 	
-	T0TCR = COUNTER0_RESET_bm; //reset
-	T0TCR = ~(COUNTER0_RESET_bm); //usuniecie flagi resetu
+	T0TCR = COUNTER0_RESET_bm;
+	T0TCR = ~(COUNTER0_RESET_bm);
 	
 	while(T0TC<((uiTime)*PCLK_FREQ)){} //zegar taktowania wynosi 1/4 taktowania rdzenia procesora, czyli 15 000 cykli na milisekunde, 15 cykli na mikrosekunde
 }
@@ -34,14 +34,17 @@ po kolei bity rejestru T0MRC (stan wysoki):
 
 void InitTimer0Match0(unsigned int iDelayTime) {
     T0MR0 = (iDelayTime * PCLK_FREQ);  // PCLK = 15 MHz, 1 µs = 15 cykli, to co wyzej
-    T0MCR = (T0MCR | (T0MCR_INTERRUPT_ON_MR0_bm | T0MCR_RESET_ON_MR0_bm)); //suma bitowa, poniewaz nie chce aby np kontrola MR1 sie zerowala
+    
+    T0MCR = (T0MCR | (T0MCR_INTERRUPT_ON_MR0_bm | T0MCR_RESET_ON_MR0_bm));
 	
     T0TCR = COUNTER0_RESET_bm;
 		T0TCR = ~(COUNTER0_RESET_bm);
+	
+    T0TCR = COUNTER0_ENABLE_bm;
 }
 
 void WaitOnTimer0Match0(void) {
     while ((T0IR & T0IR_MR0_INTERRUPT_bm) == 0) {} // Czekanie na flage przerwania w T0IR
-    T0IR = T0IR_MR0_INTERRUPT_bm; // Czyszczenie flagi przerwania (zapis 1 do bitu 0 w T0IR)
+    T0IR = (T0IR | T0IR_MR0_INTERRUPT_bm); // Czyszczenie flagi przerwania (zapis 1 do bitu 0 w T0IR)
 }
 
